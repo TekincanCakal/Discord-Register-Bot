3f8892334decd0ca0f0b905e32f72f0f9c31cf61
chasen48 <chasenfreelife@gmail.com>
2020-11-21 20:28:14 +0300

    test

---
+++ bot.js
@@ -0,0 +1,78 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.login("NzQ3ODc1ODE5NzgyOTMwNDYy.X0VPog.kFEWtSaN0UH6saxySI7qqCJvGU8");//NzQ3ODc1ODE5NzgyOTMwNDYy.X0VPog.kFEWtSaN0UH6saxySI7qqCJvGU8
+var guild;
+var manRole, womanRole;
+var unregisterRole;
+var registermanRole;
+client.on('ready', () =>
+{
+  console.log("The bot is logged in xdd");
+  client.user.setUsername("Nyän | RegisterBot");
+  guild = client.guilds.cache.get("773638840002543618");//773638840002543618
+  manRole = guild.roles.cache.get("773852113477435393");//773852113477435393
+  womanRole = guild.roles.cache.get("773852330159243274");//773852330159243274
+  unregisterRole = guild.roles.cache.get("773853246090903563");//773853246090903563
+  registermanRole = guild.roles.cache.get("773854774947872769");
+});
+client.on("message", message =>
+{
+  if(message.author.bot)
+    return;
+  if(!(message.member.roles.cache.has("773854774947872769") || message.member.roles.cache.has("773925340576874496") || message.member.roles.cache.has("773991096530632726")))
+    return;
+  if(message.channel.id === "773670491285487656" && (message.content.startsWith("!e") || message.content.startsWith("!k")))//773670491285487656
+  {
+    var args = message.content.split(" ");
+    var userId = args[1].replace("<","").replace(">","").replace("@", "").replace("!", "");
+    if(args.length !== 4)
+    {
+      message.channel.send("!(erkek/kız) @tagSomeone isim yaş");
+      return;
+    }
+    var member = message.guild.members.cache.get(userId);
+    if(member === null)
+    {
+      message.content.send("Kullanıcı bulunamadı");
+      return;
+    }
+    var username = args[2] + " | " + args[3];
+    member.setNickname(username);
+    if(args[0] === "!e")
+    {
+      member.roles.add(manRole).catch(console.error);
+    }
+    else
+    {
+      member.roles.add(womanRole).catch(console.error);
+    }
+    member.roles.remove(unregisterRole).catch(console.error);
+    message.delete({ timeout: 1000 });
+  }
+});
+client.on("guildMemberAdd", member =>
+{
+  var memberCount = guild.members.cache.filter(member => !member.user.bot).size;
+  const welcomeMessage = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setDescription("**Sunucuya Yeni Bir Üye Geldi!**")
+	.setThumbnail(member.user.avatarURL())
+	.addFields(
+		{ name: '** **', value: member.toString() + " **Hepimiz Seni Bekliyorduk.**"},
+    { name: '** **', value: "Seninle Birlikte** " + memberCount + "** Kişi Olduk! **(♡°▽°♡)**"},
+    { name: '** **', value: "Kullanıcı ID>** " + member.id + "**"},
+    { name: '** **', value: "Lütfen Yetkili Birini Etiketleyip  **İsim** ve **Yaşınızı** Yazınız."},
+    { name: '** **', value: womanRole.toString() + " Rolünü Almak İçin **SES TEYİT** Veremeniz zorunludur."},//<:kedy:779726859013324860>
+    { name: '** **', value: "**Kayıt Olma isteğiniz Kayıt Sorumlusuna iletildi.**"},//<02smug:779731824468951060>
+    { name: '** **', value: "**Kayıt Durumu; **<:x:779684693246607371>"}
+	)
+	.setTimestamp();
+  if(registermanRole.members !== null)
+  {
+    registermanRole.members.forEach(user =>
+    {
+    	user.send(member.toString() + "** Kişisi NekoNyan Sunucusuna Kayıt Olmak İstiyor Lütfen Kaydını Tamamla!**");
+    });
+  }
+  member.guild.channels.cache.get("773670491285487656").send(welcomeMessage);
+});
