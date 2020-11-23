4fd7d6e8dd01f990ed9a742adb8155c4b4b73f58
chasen48 <70641364+chasen48@users.noreply.github.com>
2020-11-23 04:41:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ function loadConfig()
 {
   fs.readFile('./config.json', 'utf8', function (err, data)
   {
-    console.log(data);
+    console.log(data.canApprove);
     configJson = JSON.parse(data);    
   });
   console.log("Config succesfully loaded!" + configJson["BotName"]);
