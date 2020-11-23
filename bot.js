ea54b263661a5f647dac993c9c5f85a2ca19a490
chasen48 <70641364+chasen48@users.noreply.github.com>
2020-11-23 04:05:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,100 +1,195 @@
 const Discord = require("discord.js");
+const fs = require('fs');
 const client = new Discord.Client();
 client.login("NzQ3ODc1ODE5NzgyOTMwNDYy.X0VPog.kFEWtSaN0UH6saxySI7qqCJvGU8");
+
+var configJson;
+var unregisterUsersJson;
 var guild;
-var manRole, womanRole;
+var manRole;
+var womanRole;
 var unregisterRole;
-var registermanRole;
+var manRegisterCommand;
+var womanRegisterCommand;
 var registerChannel;
+var commandChannel;
+var prefix;
+
+function saveConfig()
+{
+  fs.writeFile('config.json', JSON.stringify(configJson), function(err, result)
+  {
+     if(err)
+      console.log('error', err);
+  });
+}
+function loadConfig()
+{
+  fs.readFile('config.json', 'utf8', function (err, data)
+  {
+    configJson = JSON.parse(data);    
+  });
+  console.log("Config succesfully loaded!");
+}
+function updateConfig()
+{
+  guild = client.guilds.cache.get(configJson.Guild);
+  client.user.setUsername(configJson.BotName);
+  manRole = guild.roles.cache.get(configJson.ManRole);
+  womanRole = guild.roles.cache.get(configJson.WomanRole);
+  unregisterRole = guild.roles.cache.get(configJson.UnregisterRole);
+  registerChannel = guild.channels.cache.get(configJson.RegisterChannel);
+  commandChannel = guild.channels.cache.get(configJson.CommandChannel);
+  manRegisterCommand = configJson.ManRegisterCommand;
+  womanRegisterCommand = configJson.WomanRegisterCommand;
+  prefix = configJson.Prefix;
+  saveConfig();
+}
+
+function saveUsers()
+{
+  fs.writeFile('unregisterUsers.json', JSON.stringify(unregisterUsersJson), function(err, result)
+  {
+     if(err)
+      console.log('error', err);
+  });
+}
+function loadUsers()
+{
+  fs.readFile('unregisterUsers.json', 'utf8', function (err, data)
+  {
+    unregisterUsersJson = JSON.parse(data);    
+  });
+  console.log("Users succesfully loaded!");
+}
+function addUser(var user)
+{
+  var keys = Object.keys(unregisterUsersJson);
+  unregisterUsersJson["User" + keys.length] = user;
+  saveUsers();
+}
+function removeUser(var user)
+{
+  Object.keys(unregisterUsersJson).forEach(function(key)
+  {
+    if (unregisterUsersJson[key] === user.id) 
+    {
+      delete unregisterUsersJson[key];
+    }
+  });
+  saveUsers();
+}
+function userMessage(var user)
+{
+  const temp = new Discord.MessageEmbed()
+  .setColor('#0099ff')
+  .setTitle(user.id)
+  .setDescription("**Sunucuya Yeni Bir Üye Geldi!**")
+  .setThumbnail(user.avatarURL())
+  .addFields(
+    { name: '** **', value: user.toString() + "** Seni Bekliyorduk.**"},
+    { name: '** **', value: "Lütfen Yetkili Birini Etiketleyip  **İsim** ve **Yaşınızı** Yazınız."},
+    { name: '** **', value: womanRole.toString() + " Rolünü Almak İçin **SES TEYİT** Veremeniz zorunludur."},
+    { name: '** **', value: "**Kayıt Durumu; **<:x:779684693246607371>"}
+  )
+  .setTimestamp();
+  return temp;
+}
+async function printUnregisterUsers()
+{
+  registerChannel.messages.fetch({ limit: 100 }).then(async messages => 
+  {
+    for (const message of messages.array().reverse())
+    {
+      message.delete({ timeout: 0});
+    }
+  }
+  var memberCount = guild.members.cache.filter(member => !member.user.bot).size;
+  registerChannel.send("**Sunucudaki Toplam Kişi Sayısı: " memberCount + "**");
+  Object.keys(unregisterUsersJson).forEach(function(key)
+  {
+    registerChannel.send(userMessage(guild.members.cache.find(user => user.id === unregisterUsersJson[key])));
+  });
+}
 client.on('ready', () =>
 {
   console.log("Bot enabled");
-  client.user.setUsername("Nyän | Register");
-  guild = client.guilds.cache.get("773638840002543618");
-  manRole = guild.roles.cache.get("773852113477435393");
-  womanRole = guild.roles.cache.get("773852330159243274");
-  unregisterRole = guild.roles.cache.get("773853246090903563");
-  registermanRole = guild.roles.cache.get("773854774947872769");
-  registerChannel =  guild.channels.cache.get("773670491285487656");
+  loadConfig();
+  loadUsers();
 });
 client.on("message", message =>
 {
-  if(message.author.bot)
+  if(message.author.bot || (message.channel.type === "dm" || message.guild === null) ||(message.channel.id !== registerChannel.id && message.channel.id !== commandChannel.id))
     return;
-  if(!(message.member.roles.cache.has("773854774947872769") || message.member.roles.cache.has("773925340576874496") || message.member.roles.cache.has("773991096530632726")))
-    return;
-  if(message.channel.id === registerChannel.id && (message.content.startsWith("!e") || message.content.startsWith("!k")))
+  if(message.channel.id === registerChannel.id)
   {
-    var args = message.content.split(" ");
-    var userId = args[1].replace("<","").replace(">","").replace("@", "").replace("!", "");
-    if(args.length !== 4)
+    if(message.author.hasPermission("MANAGE_ROLES"))
     {
-      message.channel.send("!e @tagSomeone isim yaş | !k @tagSomeone isim yaş");
-      return;
-    }
-    var member = message.guild.members.cache.get(userId);
-    if(member === null)
-    {
-      message.channel.send("Kullanıcı bulunamadı");
-      return;
-    }
-    if(member.roles.cache.has(manRole) || member.roles.cache.has(womanRole))
-    {
-      message.channel.send("Kullanıcı zaten kayıtlı");
-      return;
-    }
-    var username = args[2] + " | " + args[3];
-    member.setNickname(username);
-    if(args[0] === "!e")
-    {
-      member.roles.add(manRole).catch(console.error);
+      var args = message.content.split(" ");
+      if(args.length === 4)
+      {
+        if(message.mentions.users.size === 1)
+        {
+          if(args[0] === prefix + ManRegisterCommand || args[0] == prefix + WomanRegisterCommand)
+          {
+            const taggedUser = message.mentions.users.first();
+            if(!taggedUser.roles.cache.has(manRole) && !taggedUser.roles.cache.has(womanRole))
+            {
+              var username = args[2] + " | " + args[3];
+              taggedUser.setNickname(username);
+              if(args[0] === prefix + ManRegisterCommand)
+              {
+                member.roles.add(manRole).catch(console.error);
+              }
+              else
+              {
+                member.roles.add(womanRole).catch(console.error);
+              }
+              member.roles.remove(unregisterRole).catch(console.error);
+              printUnregisterUsers();
+            }
+            else
+            {
+              message.reply(message.author.toString() + " Bu kullanıcı zaten kayıtlı!");
+              message.delete({ timeout: 2000 );
+            }
+          }
+          else
+          {
+            message.reply(message.author.toString() + prefix +manRegisterCommand + "/" + womanRegisterCommand + " @kişi ism yaş");
+            message.delete({ timeout: 2000 );
+          }
+        }
+        else
+        {
+          message.reply(message.author.toString() + " Kayıt etmek istediğin kişiyi taglemeyi unuttun veya 1 den fazla kişiyi tagledin!");
+          message.delete({ timeout: 2000 );
+        }
+      }
+      else
+      {
+        message.reply(message.author.toString() + prefix +manRegisterCommand + "/" + womanRegisterCommand + " @kişi ism yaş");
+        message.delete({ timeout: 2000 );
+      }
     }
     else
     {
-      member.roles.add(womanRole).catch(console.error);
+      message.reply(message.author.toString() + " Bu komutu kullanmak için yetkin yok!");
+      message.delete({ timeout: 2000 );
     }
-    member.roles.remove(unregisterRole).catch(console.error);
-    message.delete({ timeout: 1000 });
-    console.log(userId + " idli kişi kayıt oldu!");
-    message.channel.messages.fetch({ limit: 20 }).then(async messages => 
-    {
-    	for (const message of messages.array().reverse())
-	{
-		for(var i = 0; i < message.embeds.length; i++) 
-		{
-        		if(message.embeds[i].title === member.id) 
-			{
-            			message.delete({ timeout: 100 });
-    			}
-		}
-	}
-     });
+  }
+  else if(message.channel.id === commandChannel.id)
+  {
+  
   }
 });
 client.on("guildMemberAdd", member =>
 {
-  var memberCount = guild.members.cache.filter(member => !member.user.bot).size;
-  const welcomeMessage = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-  	.setTitle(member.user.id)
-	.setDescription("**Sunucuya Yeni Bir Üye Geldi!**")
-	.setThumbnail(member.user.avatarURL())
-	.addFields(
-    { name: '** **', value: member.toString() + "** Seni Bekliyorduk.**"},
-    { name: '** **', value: "Seninle Birlikte** " + memberCount + "** Kişi Olduk! **(♡°▽°♡)**"},
-    { name: '** **', value: "Kullanıcı ID>** " + member.id + "**"},
-    { name: '** **', value: "Lütfen Yetkili Birini Etiketleyip  **İsim** ve **Yaşınızı** Yazınız."},
-    { name: '** **', value: womanRole.toString() + " Rolünü Almak İçin **SES TEYİT** Veremeniz zorunludur."},
-    { name: '** **', value: "**Kayıt Olma isteğiniz Kayıt Sorumlusuna iletildi.**"},
-    { name: '** **', value: "**Kayıt Durumu; **<:x:779684693246607371>"}
-	)
-	.setTimestamp();
-  if(registermanRole.members !== null)
-  {
-    registermanRole.members.forEach(user =>
-    {
-    	user.send(member.toString() + "** Kişisi NekoNyan Sunucusuna Kayıt Olmak İstiyor Lütfen Kaydını Tamamla!**");
-    });
-  }
-  registerChannel.send(welcomeMessage);
+  addUser(member.id);
+  registerChannel.send(userMessage(member));
+});
+client.on("guildMemberRemove", member => 
+{
+  removeUser(member.id);
+  printUnregisterUsers();
 });
