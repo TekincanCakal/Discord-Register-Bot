7bd29f41a0cceaa2f3237a909722d357c8240ac0
chasen48 <70641364+chasen48@users.noreply.github.com>
2020-12-02 01:38:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ client.on("guildMemberAdd", member =>
   .setTimestamp();
   registerChannel.send(temp);
   client.user.setActivity(memberCount() + " Kişi Bu Sunucuda");
-  guild.members.cache.filter(member => !member.user.roles.cache.has(registerManRole.id)).cache.forEach(member => member.send("test")); 
+  guild.members.cache.filter(member => !member.user.roles.cache.has(registerManRole.id)).forEach(member => member.send("test")); 
   
 });
 client.on("guildMemberRemove", member => 
