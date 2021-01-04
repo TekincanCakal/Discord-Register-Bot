11266676d23a9670d53de1f68b268457d8fb2322
MrElliotAldersons <70641364+MrElliotAldersons@users.noreply.github.com>
2021-01-04 03:38:11 +0300

    s

--- bot.js
+++ bot.js
@@ -34,76 +34,64 @@ client.on("ready", () =>
 });
 client.on("message", (message) =>
 {
-  try 
-  {
-  if(message.author.bot || (message.channel.type === "dm" || message.guild === null) ||(message.channel.id !== registerChannel.id && message.channel.id !== commandChannel.id))
-    return;
-  if(message.channel.id === registerChannel.id)
-  {
-    var args = message.content.split(" ");
-    if(args[0] === configJson.Prefix + configJson.ManRegisterCommand  || args[0] === configJson.Prefix + configJson.WomanRegisterCommand)
-    {
-      if(message.member.hasPermission("MANAGE_ROLES"))
-      {
-        if(args.length === 2)
-        {
-          if(message.mentions.users.size === 1)
-          {
-            const taggedUser = guild.members.cache.get(message.mentions.users.first().id);
-            if(!(taggedUser.roles.cache.has(manRole.id) || taggedUser.roles.cache.has(womanRole.id)))
-            {
-              var roleString = "";
-              if(args[0] === configJson.Prefix + configJson.ManRegisterCommand)
-              {
-                taggedUser.roles.add(manRole).catch(console.error);
-                roleString = manRole.toString();
+  try {
+    if (message.author.bot || (message.channel.type === "dm" || message.guild === null))
+      return;
+    if (message.channel.id === registerChannel.id) {
+      var args = message.content.split(" ");
+      if (args[0] === configJson.Prefix + configJson.ManRegisterCommand || args[0] === configJson.Prefix + configJson.WomanRegisterCommand) {
+        if (message.member.hasPermission("MANAGE_ROLES")) {
+          if (args.length === 2) {
+            if (message.mentions.users.size === 1) {
+              const taggedUser = guild.members.cache.get(message.mentions.users.first().id);
+              if (!(taggedUser.roles.cache.has(manRole.id) || taggedUser.roles.cache.has(womanRole.id))) {
+                var roleString = "";
+                if (args[0] === configJson.Prefix + configJson.ManRegisterCommand) {
+                  taggedUser.roles.add(manRole).catch(console.error);
+                  roleString = manRole.toString();
+                } else {
+                  taggedUser.roles.add(womanRole).catch(console.error);
+                  roleString = womanRole.toString();
+                }
+                taggedUser.roles.remove(unregisterRole).catch(console.error);
+                taggedUser.roles.add(registerRole).catch(console.error);
+                const temp = new Discord.MessageEmbed()
+                    .setColor('#000000')
+                    .setAuthor("Nyän | Kayıt Sistemi", client.user.avatarURL(), "")
+                    .setTitle("Kayıt Tamamlandı")
+                    .setDescription(":sparkles:**Kayıt Edilen Kullanıcı:**" + taggedUser.user.toString() + "\n:boom: **Verilen Rol:**" + roleString + "\n:rice_ball: **Kayıt Eden Yetkili: **" + message.author.toString())
+                    .setThumbnail(taggedUser.user.avatarURL())
+                    .setTimestamp();
+                registerChannel.send(temp);
+                message.delete({timeout: 100});
+              } else {
+                message.reply("Bu kullanıcı zaten kayıtlı!").then(msg => {
+                  msg.delete({timeout: 1000})
+                }).catch(console.error);
+                message.delete({timeout: 1000});
               }
-              else
-              {
-                taggedUser.roles.add(womanRole).catch(console.error);
-                roleString = womanRole.toString();
-              }
-              taggedUser.roles.remove(unregisterRole).catch(console.error);
-              taggedUser.roles.add(registerRole).catch(console.error);
-              const temp = new Discord.MessageEmbed()
-              .setColor('#000000')
-              .setAuthor("Nyän | Kayıt Sistemi", client.user.avatarURL(), "")
-              .setTitle("Kayıt Tamamlandı")
-              .setDescription(":sparkles:**Kayıt Edilen Kullanıcı:**" + taggedUser.user.toString() + "\n:boom: **Verilen Rol:**" + roleString + "\n:rice_ball: **Kayıt Eden Yetkili: **" + message.author.toString())
-              .setThumbnail(taggedUser.user.avatarURL())
-              .setTimestamp();
-              registerChannel.send(temp);
-              message.delete({ timeout: 100});
+            } else {
+              message.reply("Kayıt etmek istediğin kişiyi taglemeyi unuttun veya 1 den fazla kişiyi tagledin!").then(msg => {
+                msg.delete({timeout: 1000})
+              }).catch(console.error);
+              message.delete({timeout: 1000});
             }
-            else
-            {
-              message.reply("Bu kullanıcı zaten kayıtlı!").then(msg => {msg.delete({ timeout: 1000 })}).catch(console.error);
-              message.delete({ timeout: 1000});
-            }
+          } else {
+            message.reply(configJson.Prefix + configJson.ManRegisterCommand + "/" + configJson.WomanRegisterCommand + "@kişi").then(msg => {
+              msg.delete({timeout: 1000})
+            }).catch(console.error);
+            message.delete({timeout: 1000});
           }
-          else
-          {
-            message.reply("Kayıt etmek istediğin kişiyi taglemeyi unuttun veya 1 den fazla kişiyi tagledin!").then(msg => {msg.delete({ timeout: 1000 })}).catch(console.error);
-            message.delete({ timeout: 1000});
-          }
+        } else {
+          message.reply("Bu komutu kullanmak için yetkin yok!").then(msg => {
+            msg.delete({timeout: 1000})
+          }).catch(console.error);
+          message.delete({timeout: 1000});
         }
-         else
-          {
-            message.reply(configJson.Prefix + configJson.ManRegisterCommand + "/" + configJson.WomanRegisterCommand + "@kişi").then(msg => {msg.delete({ timeout: 1000 })}).catch(console.error);
-            message.delete({ timeout: 1000});
-          }
       }
-      else
-      {
-        message.reply("Bu komutu kullanmak için yetkin yok!").then(msg => {msg.delete({ timeout: 1000 })}).catch(console.error);
-        message.delete({ timeout: 1000});
-      } 
     }
-  }
-    if(message.author.id === "339408846705524737")
-    {
-      if(message.content.startsWith("!nyansay"))
-      {
+    if (message.author.id === "339408846705524737") {
+      if (message.content.startsWith("!nyansay")) {
         var temp = message.content.replace("!nyansay ", "");
         message.channel.send(temp);
         console.log(temp);
@@ -122,7 +110,7 @@ client.on("guildMemberAdd", member =>
   const temp = new Discord.MessageEmbed()
  .setColor('#000000')
  .setAuthor("Nyän | Kayıt Sistemi", client.user.avatarURL(), "")
- .setDescription("<:kedy:784386078949769218> **Sunucumuza Hoşgeldin**" + member.user.toString() +"\n:date: **Hesap Oluşturma Tarihi:** " + date + "\n**:white_check_mark: Güvenilirlik Durumu:** Güvenilir\n:cherry_blossom: Kayıt için teyit zorunludur\n:stars: **Kayıt olmak için yetkilileri beklemen yeterlidir.**\n**Yetkililer sizinle ilgilenecektir.**")
+ .setDescription("<:kedy:794319006068113408> **Sunucumuza Hoşgeldin**" + member.user.toString() +"\n:date: **Hesap Oluşturma Tarihi:** " + date + "\n**:white_check_mark: Güvenilirlik Durumu:** Güvenilir\n:cherry_blossom: Kayıt için teyit zorunludur\n:stars: **Kayıt olmak için yetkilileri beklemen yeterlidir.**\n**Yetkililer sizinle ilgilenecektir.**")
  .setThumbnail(member.user.avatarURL());
   registerChannel.send(temp);
   client.user.setActivity(memberCount() + " Kişi Bu Sunucuda");
