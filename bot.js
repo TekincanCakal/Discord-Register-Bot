7bc69d348b2a2593a5e9f541fadb092f560a3934
MrElliotAldersons <70641364+MrElliotAldersons@users.noreply.github.com>
2021-01-30 16:52:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login("NzQ3ODc1ODE5NzgyOTMwNDYy.X0VPog.avWW-zeaYUqEwrAnAAL5x11zCDo");
+//client.login("NzQ3ODc1ODE5NzgyOTMwNDYy.X0VPog.avWW-zeaYUqEwrAnAAL5x11zCDo");
 var configJson = require("./config.json");
 var guild;
 var manRole;
